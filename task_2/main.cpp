cb2e1023ca89d0609fd2c6d0ad7ba89f3add5d7e
Dian Karyoko <diankaryoko@gmail.com>
2025-07-21 22:19:10 +0700

    refactor: extract certain group of logics into separate function

--- task_2/main.cpp
+++ task_2/main.cpp
@@ -4,57 +4,72 @@
 using namespace std;
 
 int main() {
-    int chosen_number;
-    
+    int chosenNumber;
+
     do {
-        srand(time(0));
+        displayMenu();
+        cin >> chosenNumber;
 
-        int random_number = (rand() % 50) + 1;
-        int guess = 0;
-        
-        cout << "====== Number Guessing Game ======" << endl;
-        cout << "1. Start Game" << endl;
-        cout << "2. Exit" << endl;
-        cout << "Choose an option: ";
-        cin >> chosen_number;
-
-        switch(chosen_number) {
+        switch(chosenNumber) {
             case 1: 
-                cout << "\nI have selected a number between 1 and 50. You have 5 tries.\n\n";
-                for(int attempt = 1; attempt < 6; attempt++) {
-                    cout << "Attempt " << attempt << ": Enter your guess: ";
-                    cin >> guess;
-
-                    if(guess < 1 || guess > 50) {
-                        cout << "Your guess is out of range. The selected number is between 1 and 50. Try again!\n\n";
-                        attempt--;
-                        continue;
-                    }
-                    
-                    if(guess == random_number) {
-                        cout << "Congratulations! You guessed it in " << attempt << " attempts!\n\n";
-                        break;
-                    }
-                    else if(guess > random_number) {
-                        cout << "Too high!\n\n";
-                    }
-                    else {
-                        cout << "Too low!\n\n";
-                    }
-                    
-                    if(attempt == 5) {
-                        cout << "Sorry, you've used all your attempts. The correct number was: " << random_number << "\n\n";
-                    }
-                }
+                startGame();
                 break;
             case 2: 
-                cout << "Bye, see you next time!\n\n";
+                exitGame();
                 break;
             default:
                 cout << "Invalid input. Please only choose number 1 or 2.\n\n";
                 break;
         }
-    } while(chosen_number != 2);
+    } while(chosenNumber != 2);
 
     return 0;
+}
+
+void displayMenu() {
+    cout << "====== Number Guessing Game ======" << endl;
+    cout << "1. Start Game" << endl;
+    cout << "2. Exit" << endl;
+    cout << "Choose an option: ";
+}
+
+void startGame() {
+    cout << "\nI have selected a number between 1 and 50. You have 5 tries.\n\n";
+
+    srand(time(0));
+    int randomNumber = (rand() % 50) + 1;
+    int guess = 0;
+
+    for(int attempt = 1; attempt < 6; attempt++) {
+        cout << "Attempt " << attempt << ": Enter your guess: ";
+        cin >> guess;
+
+        // Validate number range
+        if(guess < 1 || guess > 50) {
+            cout << "Your guess is out of range. The selected number is between 1 and 50. Try again!\n\n";
+            attempt--;
+            continue;
+        }
+        
+        // Validate guess
+        if(guess == randomNumber) {
+            cout << "Congratulations! You guessed it in " << attempt << " attempts!\n\n";
+            break;
+        }
+        else if(guess > randomNumber) {
+            cout << "Too high!\n\n";
+        }
+        else {
+            cout << "Too low!\n\n";
+        }
+        
+        // Game over
+        if(attempt == 5) {
+            cout << "Sorry, you've used all your attempts. The correct number was: " << randomNumber << "\n\n";
+        }
+    }
+}
+
+void exitGame() {
+    cout << "Bye, see you next time!\n\n";
 }
